565ead4cfce8894e4ee8679b9f501eb746987970
Olivier Albiez <olivier.albiez@poloper.org>
2014-11-14 21:33:46 +0100

    goserve now log access

--- goserve/goserve.go
+++ goserve/goserve.go
@@ -2,7 +2,10 @@ package main
 
 import (
 	"flag"
+	"github.com/gorilla/handlers"
+	"log"
 	"net/http"
+	"os"
 )
 
 var port = flag.String("port", "8080", "Define what TCP port to bind to")
@@ -10,5 +13,6 @@ var root = flag.String("root", ".", "Define the root filesystem path")
 
 func main() {
 	flag.Parse()
-	panic(http.ListenAndServe(":"+*port, http.FileServer(http.Dir(*root))))
+	log.Printf("About to serve '" + *root + "' on port " + *port)
+	panic(http.ListenAndServe(":"+*port, handlers.LoggingHandler(os.Stdout, http.FileServer(http.Dir(*root)))))
 }
